18c77ce88645b9998a376d1dd6faef939d0edc3a
David Verghese <dj.verghese@gmail.com>
2023-01-31 21:46:19 -0500

    add different size columns

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let config = {
   renderer: Phaser.AUTO,
   width: 800,
@@ -18,12 +19,11 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
-
 function preload ()
 {
   this.load.image('background', 'assets/background.png');
   this.load.image('road', 'assets/road.png');
-  this.load.image('column', 'assets/column.png');
+  this.load.image('column', 'assets/column32x200.png');
 }
 
 let platforms;
@@ -32,18 +32,20 @@ function create ()
   this.add.image(0, 0, 'background').setOrigin(0, 0);
 
   platforms = this.physics.add.staticGroup();
+  platforms.create(50, 0, 'column').setOrigin(0, 0);
 
-  for (let xPosition = 50; xPosition < 700; xPosition += 200){
-    const diff = Math.random() * 0.7;
-    if (Math.random() > 0.5) {
-      platforms.create(xPosition, 500, 'column').setScale(1,[1+diff]);
-      platforms.create(xPosition, 0, 'column').setScale(1,[1-diff]);
-    } 
-    else {
-      platforms.create(xPosition, 500, 'column').setScale(1,[1+diff]);
-      platforms.create(xPosition, 0, 'column').setScale(1,[1-diff]);
-    }
-  }
+
+  // for (let xPosition = 50; xPosition < 700; xPosition += 200){
+  //   const diff = Math.random() * 0.7;
+  //   if (Math.random() > 0.5) {
+  //     platforms.create(xPosition, 500, 'column').setScale(1,[1+diff]);
+  //     platforms.create(xPosition, 0, 'column').setScale(1,[1-diff]);
+  //   } 
+  //   else {
+  //     platforms.create(xPosition, 500, 'column').setScale(1,[1+diff]);
+  //     platforms.create(xPosition, 0, 'column').setScale(1,[1-diff]);
+  //   }
+  // }
 
   platforms.create(400, 568, 'road').setScale(2).refreshBody();
   
@@ -52,4 +54,3 @@ function create ()
 function update ()
 {
 }
-
