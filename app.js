0af11a37f658baa5e26b56d51859e8f192bee302
David Verghese <dj.verghese@gmail.com>
2023-02-01 12:38:05 -0500

    create system of generating ccolumns

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ function preload ()
 {
   this.load.image('background', 'assets/background.png');
   this.load.image('road', 'assets/road.png');
-  this.load.image('column', 'assets/column.png');
+  this.load.image('column32By100', 'assets/column32x100.png');
+  this.load.image('column32By200', 'assets/column32x200.png');
+  this.load.image('column32By300', 'assets/column32x300.png');
+  this.load.image('column32By400', 'assets/column32x400.png');
+  this.load.image('column32By500', 'assets/column32x500.png');
 }
 
 let platforms;
@@ -34,10 +38,11 @@ function create ()
   platforms = this.physics.add.staticGroup();
   
   for (let i = -100; i > -800; i -= 100){
-    const resize = Math.round(Math.random() * 4)+1;
-    console.log(resize, 6 - resize);
-    const top = platforms.create(0, 0, 'column').setScale(1,[resize]);;
-    const bottom = platforms.create(0, 0, 'column').setScale(1, [6 - resize]);
+    let picker = Math.round(Math.random() * 2);
+    const columnPairings = [['column32By300', 'column32By100'], ['column32By200', 'column32By200'], ['column32By100', 'column32By300']];
+    const top = platforms.create(0, 0, columnPairings[picker][0]);
+    const bottom = platforms.create(0, 0, columnPairings[picker][1]);
+
     // https://phaser.io/examples/v3/category/display/align
     // https://stackoverflow.com/questions/63978497/phaserjs-3-0-how-to-place-image-in-right-bottom-of-the-screen
     Phaser.Display.Align.In.TopLeft(top, background,i);
